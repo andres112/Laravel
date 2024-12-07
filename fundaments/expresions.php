a4fca1b63f8b83a89da4af89f3af952ea4e68963
Andres Felipe Dorado <andres.dorado90@gmail.com>
2024-12-07 15:00:24 +0100

    feat: enhance expresions.php with concatenation and data types

--- fundaments/expresions.php
+++ fundaments/expresions.php
@@ -4,3 +4,16 @@ $name = "John";
 $lastName = "Doe";
 
 echo "Hello $name $lastName!\n";
+
+echo "Concatenation: " . $name . " - " . $lastName . "!\n";
+
+$pizzas = 2;
+$slices = 8;
+$totalSlices = $pizzas * $slices;
+
+echo "I have $pizzas pizzas and $slices slices each, total slices: $totalSlices\n";
+
+// Set randomly true or false
+$isHungry = rand(0, 1) == 1;
+
+echo "Are you hungry? " . ($isHungry ? "Yes" : "No") . "\n";
