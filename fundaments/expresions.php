d8a073e845dd6015055ba7b73accc2c00b6bb78c
Andres Felipe Dorado <andres.dorado90@gmail.com>
2024-12-08 10:34:25 +0100

    feat: enhance expresions.php with improved formatting for data types and condition statements

--- fundaments/expresions.php
+++ fundaments/expresions.php
@@ -18,7 +18,8 @@ $isHungry = rand(0, 1) == 1;
 
 echo "Are you hungry? " . ($isHungry ? "Yes" : "No") . "\n";
 
-echo "\nDATA TYPES\n";
+echo "************************************\n";
+echo "DATA TYPES\n";
 
 $isStudent = 1;
 echo "var_dump(variable): \n";
@@ -26,3 +27,6 @@ var_dump($isStudent, $isStudent == true, $isStudent === true); # var_dump shows 
  echo "\n";
 $scores = [10, '20', 5.55, (int)5.55];
 var_dump($scores, $scores[0], $scores[1], $scores[2], $scores[3], $scores[0] + $scores[1]);
+
+echo "************************************\n";
+echo "CONDITION IF STATEMENT\n";
