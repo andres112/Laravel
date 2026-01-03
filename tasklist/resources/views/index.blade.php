e2a5c4a8f3ea946f1c32aa813f2c1d80588a96c7
Andres Felipe Dorado <andres.dorado90@gmail.com>
2026-01-03 17:06:42 +0100

    refactor: update blade templates for consistency and clarity

--- tasklist/resources/views/index.blade.php
+++ tasklist/resources/views/index.blade.php
@@ -1,5 +1,7 @@
+@extends('layouts.app')
+
 {{-- Create a simple Home page template in blade.php file. --}}
-<h1>Welcome to the Task List</h1>
+@section('title', 'The list of recently edited tasks')
 
 {{-- Debug output --}}
 <div style="background: #f0f0f0; padding: 10px; margin: 10px 0; border: 1px solid #ccc;">
@@ -11,29 +13,31 @@
     @endif
 </div>
 
-<div>
-    <h3>Recently Edited Tasks:</h3>
-    <ul>
-        @forelse ($tasks as $task)
-            <li>
-                <strong> <a href="{{ route('tasks.show', ['taskId' => $task->id]) }}">{{ $task->title }}</a></strong> -
-                Last Edited:
-                {{ $task->updated_at->format('Y-m-d H:i:s') }}
-            </li>
-        @empty
-            <li>No tasks available.</li>
-        @endforelse
-    </ul>
-</div>
+@section('content')
+    <div>
+        <h3>Recently Edited Tasks:</h3>
+        <ul>
+            @forelse ($tasks as $task)
+                <li>
+                    <strong> <a href="{{ route('tasks.show', ['taskId' => $task->id]) }}">{{ $task->title }}</a></strong> -
+                    Last Edited:
+                    {{ $task->updated_at->format('Y-m-d H:i:s') }}
+                </li>
+            @empty
+                <li>No tasks available.</li>
+            @endforelse
+        </ul>
+    </div>
 
-{{-- Country flag, just for studying purposes: --}}
-@if (request()->has('code'))
-    <h2 style="display: inline-flex; gap: 1rem; align-items: center;">
-        <img src="https://flagcdn.com/{{ strtolower(request('code')) }}.svg" alt="{{ strtoupper(request('code')) }} Flag"
-            width="300">
-        {{-- @else
+    {{-- Country flag, just for studying purposes: --}}
+    @if (request()->has('code'))
+        <h2 style="display: inline-flex; gap: 1rem; align-items: center;">
+            <img src="https://flagcdn.com/{{ strtolower(request('code')) }}.svg" alt="{{ strtoupper(request('code')) }} Flag"
+                width="300">
+            {{-- @else
         @isset($country)
         Country: {{ $country }}
         @endisset --}}
-    </h2>
-@endif
+        </h2>
+    @endif
+@endsection
--- tasklist/resources/views/show.blade.php
+++ tasklist/resources/views/show.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @section('title', $task->title)
-@endsection
 
 @section('content')
     {{-- Comeback button --}}
