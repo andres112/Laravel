9945cfb7b8677e61e22e8d5dcdc7239622895dfb
Andres Felipe Dorado <andres.dorado90@gmail.com>
2026-01-04 16:02:37 +0100

    feat: implement Task model factory and update DatabaseSeeder for task creation

--- tasklist/database/seeders/DatabaseSeeder.php
+++ tasklist/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Task;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,11 +16,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Create user without persisting to DB with make()
+        User::factory(1)->make();
+        // Create 20 tasks persisting in DB with create()
+        Task::factory(20)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
